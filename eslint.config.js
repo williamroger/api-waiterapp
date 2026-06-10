13858d28d3f4e126a80f71da7369c618914fdf29
William Roger <williamroger.frontend@gmail.com>
2026-06-10 01:00:37 -0300

    add mongodb connection

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
     rules: {
-      "indent": ["error", 2],
-      "no-undef": "error",
-      "no-unused-vars": "error",
-      "linebreak-style": ["error", "unix"],
-      "quotes": ["error", "single"],
-      "semi": ["error", "always"],
+      'indent': ['error', 2],
+      'no-undef': 'error',
+      'no-unused-vars': 'error',
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
     },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: { globals: globals.node }
   },
   tseslint.configs.recommended,
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-const port = 3001;
+    app.listen(port, () => {
+      console.log(`🚀 Server is running on http://localhost:${port}`);
+    });
 
-app.listen(port, () => {
-  console.log(`🚀 Server is running on http://localhost:${port}`);
-});
+    console.log('Conectado ao MongoDB.')
+  })
+  .catch(() => console.log('Erro ao tentar conectar com o MongoDB.'))
