1d34a05c28ca4ad81a7f0b8a449af5c155f35650
William Roger <williamroger.frontend@gmail.com>
2026-06-24 23:18:00 -0300

    add static path middleware to show images

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
+import path from 'node:path';
 import express from 'express';
 import mongoose from 'mongoose';
 
 import { router } from './router.ts';
+
+const currentDir = import.meta.dirname;
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
     const port = 3001;
 
+    app.use('/uploads', express.static(path.resolve(currentDir, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
 
